54c1c0f9812f4a18b66e8a511893ba68c5bd2d20
Shekhar <pythonic@gmail.com>
2022-11-24 19:09:01 +0530

    using f strings

--- converge/settings.py
+++ converge/settings.py
@@ -13,14 +13,14 @@ RC_FILENAME = '.convergerc'
 
 
 def print_and_exit(msg):
-    print('ERROR: ' + msg)
+    print(f'ERROR: {msg}')
     sys.exit(1)
 
 
 def run_command(cmd, ignore_failure=False):
     ret = os.system(cmd)
     if not ignore_failure and (ret != 0):
-        print_and_exit('[%s] exited with status %s' % (cmd, ret))
+        print_and_exit(f'[{cmd}] exited with status {ret}')
 
 
 def parse_osenv(rc_config):
@@ -41,10 +41,10 @@ def import_settings(name, settings_dir=None, exit_on_err=False):
         spec.loader.exec_module(mod)
         ns.update(dict((name, getattr(mod, name))
                   for name in dir(mod) if not name.startswith('_')))
-        print('[INFO] successfully imported: %s' % name)
+        print(f'[INFO] successfully imported: {name}')
     except Exception as err:
         level = 'Error' if exit_on_err else 'Warning'
-        print('[%s] Could not import "%s": %s' % (level, path, err))
+        print(f'[level] Could not import "{path}": {err}')
         if exit_on_err:
             sys.exit(1)
 
@@ -52,19 +52,18 @@ def import_settings(name, settings_dir=None, exit_on_err=False):
 def validate_mode(mode):
     supported_app_modes = ('prod', 'test', 'dev', 'staging', 'beta')
     if mode not in supported_app_modes:
-        print_and_exit('ERROR: unsupported mode: %s not in %s' %
-                       (mode, supported_app_modes))
-    print('INFO: APP will run in [%s] mode' % mode)
+        print_and_exit(f'ERROR: unsupported mode: {mode} not in {supported_app_modes}')
+    print(f'INFO: APP will run in [{mode}] mode')
     return mode
 
 
 def clone_git_repo(git_url, settings_dir, git_subdir=None):
 
     with tempfile.TemporaryDirectory() as temp_dir:
-        clone_cmd = 'git clone %s %s' % (git_url, temp_dir)
+        clone_cmd = f'git clone {git_url} {temp_dir}'
         src_dir = os.path.join(temp_dir, git_subdir) if git_subdir \
             else temp_dir
-        cp_cmd = 'cp -rvf %s/*_settings.py %s' % (src_dir, settings_dir)
+        cp_cmd = f'cp -rvf {src_dir}/*_settings.py {settings_dir}'
 
         run_command(clone_cmd)
         run_command(cp_cmd)
@@ -95,7 +94,7 @@ def main():
 
     if rc_config['GIT_SETTINGS_REPO']:
         if not os.path.exists(settings_dir):
-            print('Creating directory: %s' % settings_dir)
+            print(f'Creating directory: {settings_dir}')
             os.mkdir(settings_dir)
         clone_git_repo(git_url, settings_dir, git_subdir)
 
